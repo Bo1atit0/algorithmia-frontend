e28c6c04a35120d6068b6ecfc75b6e3b9d96e92c
bo1atito <shorungbebolatito@gmail.com>
2025-09-19 22:38:16 +0100

    completed the desktop navbar, working on the mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
   const NavItems = [
     'Home',
     'Projects',
@@ -9,24 +10,52 @@ const Navbar = () => {
     'Contribute',
     'Contact',
   ];
+
   return (
-    <nav className="flex justify-between items-center px-16 py-6">
-      {/* image */}
-      <div className="h-3 w-3 p-5 bg-indigo-500 rounded-lg text-lg font-bold flex justify-center items-center text-gray-300">
-        AL
-      </div>
+    <>
+      <nav className="flex justify-between md:items-center px-10 py-8 md:px-16 md:py-6">
+        {/* image */}
+        <div className="h-3 w-3 p-5 bg-indigo-500 rounded-lg text-lg font-bold flex justify-center items-center text-gray-300">
+          AL
+        </div>
 
-      {/* nav list */}
-      <ul className="flex gap-8 font-medium text-gray-800 ">
-        {NavItems.map((item, index) => (
-          <li className="cursor-pointer hover:border-b-2" key={index}>
-            {item}
-          </li>
-        ))}
-      </ul>
+        {/* Desktop nav list */}
+        <ul className="hidden md:flex lg:flex gap-8 font-medium text-gray-800 ">
+          {NavItems.map((item, index) => (
+            <li
+              className="cursor-pointer 
+            relative
+            after:absolute
+            after:left-0
+            after:-bottom-2
+            after:w-full
+            after:h-0.5
+            after:opacity-0
+            hover:after:opacity-95
+            after:bg-[linear-gradient(to_right,indigo_0_40%,transparent_40%),repeating-linear-gradient(to_right,indigo_0_20%,transparent_20%_25%)]
+            after:transition-all 
+            after:duration-1000 
+            after:ease-in-out
+            after:scale-x-0
+            hover:after:scale-x-100
+            after:origin-left
+            "
+              key={index}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
 
-      {/* buttons */}
-      {/* <div className="flex gap-4">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="relative md:hidden mb-0"
+        >
+          {isOpen ? 'X' : '+'}
+        </button>
+
+        {/* buttons */}
+        {/* <div className="flex gap-4">
         <button className="border-2 px-4 py-2 font-medium rounded-lg">
           GitHub
         </button>
@@ -34,7 +63,17 @@ const Navbar = () => {
           Join Community
         </button>
       </div> */}
-    </nav>
+      </nav>
+
+      {/* mobile list */}
+      {isOpen && (
+        <ul className="absolute right-0 px-8 mx-auto  border bg-gray-300 w-full p">
+          {NavItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
