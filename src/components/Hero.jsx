d072e8dfe513417a07c577771b049774a34ebe7b
bo1atito <shorungbebolatito@gmail.com>
2025-10-10 14:13:00 +0100

    worked on the hero section image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,19 +5,19 @@ import Button from './Button';
 
 const Hero = () => {
   return (
-    <section>
+    <section className="py-20">
       {/* hero section */}
-      <div className="flex flex-col md:flex-row items-center text-gray-800">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-0 items-center text-gray-800">
         {/* Text */}
         <div className=" flex flex-col md:w-[50vw] gap-8 items-center justify-center md:ml-10">
-          <h1 className="text-4xl md:text-7xl font-extrabold text-center ">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-center ">
             Building Africa's
             <br />
-            <span className="text-c2 ">Open Source</span>
+            <span className="text-c2 font-bold"> Open Source </span>
             <br />
             Future
           </h1>
-          <p className="px-4 text-center text-xl font-black">
+          <p className="px-4 text-center sm:text-lg md:text-xl font-black">
             Join Algorithmia SE, a vibrant community of developers, designers,
             and innovators creating open source solutions that address real
             challenges across Africa.
@@ -36,8 +36,12 @@ const Hero = () => {
         </div>
 
         {/* Image */}
-        <div className=" flex items-center justify-center">
-          <img src={HeroImage} alt="Hero Image" className="md:h-[80vh] " />
+        <div className=" flex items-center justify-center border-4 border-c1 rounded-full">
+          <img
+            src={HeroImage}
+            alt="Hero Image"
+            className="md:w-[40vw] w-[70vw] rounded-full"
+          />
         </div>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { RiUserCommunityLine } from 'react-icons/ri';
+import { Link } from 'react-scroll';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+
   const NavItems = [
-    'Home',
-    'Projects',
-    'Blog',
-    'About',
-    'Contribute',
-    'Contact',
+    { href: 'home', label: 'Home' },
+    { href: 'projects', label: 'Projects' },
+    { href: 'blog', label: 'Blog' },
+    { href: 'about', label: 'About' },
+    { href: 'contribuute', label: 'Contribute' },
+    { href: 'contact', label: 'Contact' },
   ];
 
   return (
-    <section>
-      <nav className=" py-5 md:py-4 fixed top-0 left-0 right-0 backdrop-blur-2xl px-4 md:px-0 z-20">
+    <section className="h-10 border">
+      <nav className=" py-3 md:py-5 fixed top-0 left-0 right-0 backdrop-blur-2xl px-4 md:px-6 z-20 ">
         <div className="flex justify-between items-center max-w-6xl mx-auto">
           {/* image */}
           <div className="h-3 w-3 p-5 bg-c1 rounded-lg text-lg font-bold flex justify-center items-center text-gray-300">
@@ -22,29 +25,37 @@ const Navbar = () => {
 
           {/* Desktop nav list */}
           <div className="hidden md:flex lg:flex items-center gap-8 justify-between font-medium text-gray-800 ">
-            {NavItems.map((item, index) => (
-              <a
-                className="cursor-pointer 
+            {NavItems.map((links, index) => (
+              <Link
+                to={links.href}
+                onClick={() => setActiveLink(links.href)}
+                className={`cursor-pointer 
             relative
             after:absolute
             after:left-0
             after:-bottom-2
-            after:w-full
+            ${
+              activeLink === links.href
+                ? 'after:w-full text-neutral-900  after:opacity-100 after:scale-x-100'
+                : 'text-neutral-700 hover:after:w-full  hover:after:opacity-95'
+            }
             after:h-0.5
+            after:w-0
             after:opacity-0
-            hover:after:opacity-95
+            after:scale-x-0
+            hover:after:scale-x-100
             after:bg-[linear-gradient(to_right,indigo_0_40%,transparent_40%),repeating-linear-gradient(to_right,indigo_0_20%,transparent_20%_25%)]
             after:transition-all 
             after:duration-1000 
             after:ease-in-out
-            after:scale-x-0
-            hover:after:scale-x-100
+            
+            
             after:origin-left
-            "
+            `}
                 key={index}
               >
-                {item}
-              </a>
+                {links.label}
+              </Link>
             ))}
           </div>
           <button className="hidden md:flex items-center gap-2 bg-c2 px-4 py-2 font-semibold rounded-lg text-neutral-900 ">
@@ -74,18 +85,18 @@ const Navbar = () => {
       {/* mobile list */}
       <div className=" flex justify-center p-20">
         {/* {isOpen && ( */}
-        <ul
+        <div
           className={`flex flex-col gap-1 
           md:hidden absolute top-0 bg-gray-300/50 w-90 px-6 py-3 space-y-2
           transition-all duration-1000 ease-in-out
           ${isOpen ? 'translate-y-20 opacity-100' : 'translte-y-0 opacity-0'}`}
         >
-          {NavItems.map((item, index) => (
-            <li key={index} className="w-full text-lg font-semibold">
-              {item}
-            </li>
+          {NavItems.map((links, index) => (
+            <Link key={index} className="w-full text-lg font-semibold">
+              {links.label}
+            </Link>
           ))}
-        </ul>
+        </div>
         {/* )} */}
       </div>
     </section>
