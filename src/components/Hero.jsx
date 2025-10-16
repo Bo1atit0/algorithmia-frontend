c17d7bb805cc183850c002d5a1e41c0ae9c00305
bo1atito <shorungbebolatito@gmail.com>
2025-10-16 23:13:27 +0100

    feat:added a circular border with rotating animations to the hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
-import HeroImage from '../assets/heroImg.jpg';
+import HeroImage from '../assets/heroImg.webp';
 import { FaGithub } from 'react-icons/fa';
 import { FaAngleRight } from 'react-icons/fa';
 import Button from './Button';
 
 const Hero = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 flex flex-col">
       {/* hero section */}
-      <div className="flex flex-col md:flex-row gap-4 md:gap-0 items-center text-gray-800">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 items-center text-gray-800">
         {/* Text */}
         <div className=" flex flex-col md:w-[50vw] gap-8 items-center justify-center md:ml-10">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-center ">
@@ -33,15 +33,41 @@ const Hero = () => {
               Explore Projects
             </Button>
           </div>
+
+          <div className="flex justify-center items-center gap-8">
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-4xl font-bold text-c2">50+</p>
+              <p className="text-xl text-neutral-700">Active Contributors</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-4xl font-bold text-c2">12</p>
+              <p className="text-xl text-neutral-700">Open Projects</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-4xl font-bold text-c2">8</p>
+              <p className="text-xl text-neutral-700">Countries</p>
+            </div>
+          </div>
         </div>
 
         {/* Image */}
-        <div className=" flex items-center justify-center border-4 border-c1 rounded-full">
+        <div
+          className=" p-1 rounded-full relative z-10 overflow-hidden
+        after:bg-[conic-gradient(transparent,transparent,transparent,var(--color-c1))]
+        after:content-[''] after:absolute after:animate-[spinSlow_10s_linear_infinite]
+        after:z-[-1] after:inset-0 after:[animation-delay:-5s]
+        before:content-[''] before:absolute before:inset-0 before:z-[-1] before:animate-[spinSlow_10s_linear_infinite]
+        before:bg-[conic-gradient(transparent,transparent,transparent,var(--color-c1))]
+        "
+        >
           <img
             src={HeroImage}
             alt="Hero Image"
-            className="md:w-[40vw] w-[70vw] rounded-full"
+            className="md:w-[38vw] w-[70vw] rounded-full"
           />
+          {/* <div className="bg-neutral-300 absolute inset-0 mix-blend-darken"></div> */}
         </div>
       </div>
     </section>
